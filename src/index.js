0fb0ff3448712e22d1c776712960ea8746e820be
Syarguk <bulbaburak@gmail.com>
2022-01-08 00:01:31 +0300

    fix: change index file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0) n = -n;
+    let res = "";
+    let strN = String(n);
+    let i = strN.length;
+    while(i > 0) {
+        res += strN[i - 1];
+        i--;
+    };
+    return Number(res);
 }
